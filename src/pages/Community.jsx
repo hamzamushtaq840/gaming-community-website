1577704e3dad666e4c25fa50c7ba57dc875c9b65
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-04-27 17:39:12 +0500

    fixes

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import GoalsCard from '../components/Community/GoalsCard'
 import UpcommingEventIcon from '../assets/UpcommingEventIcon.svg'
+import FooterBackgroundImage from '../assets/footerBackgroundImage.png'
+import JobListingsIcon from '../assets/JobListingsIcon.svg'
 import { communityGoalsCardsData } from "../utils/consts.js"
 import FeaturedProjectsCard from '../components/Community/FeaturedProjectsCard'
 import UpcommingEventCard from '../components/Community/UpcommingEventCard'
+import JobListingCard from '../components/Community/JobListingCard'
+import { jobListingData } from "../utils/consts.js";
 
 export default function Community() {
   return (
@@ -45,18 +49,44 @@ export default function Community() {
 
       <div className='w-full min-h-[1087px] bg-[#F5F5F5]'>
         <div className='w-[1234px] xsm:w-[64.27083333333333vw] sm:w-[64.27083333333333vw] md:w-[64.27083333333333vw] lg:w-[64.27083333333333vw] min-w-[300px] min-h-[940px] m-auto pt-[73px] pb-[108px]'>
-            <div className='flex gap-[18px] items-center'>
-              <img src={UpcommingEventIcon}/>
-              <h3 className='font-[700] text-[30px] leading-[34.35px]'>Upcoming Events</h3>
-            </div>
-            <div className='mt-[37px] flex gap-[18px] flex-wrap'>
-              <UpcommingEventCard background={"#EC1F26"}/>
-              <UpcommingEventCard background={"#F48589"}/>
-              <UpcommingEventCard background={"#EC1F26"}/>
-              <UpcommingEventCard background={"#F48589"}/>
-              <UpcommingEventCard background={"#EC1F26"}/>
-              <UpcommingEventCard background={"#F48589"}/>
-            </div>
+          <div className='flex gap-[18px] items-center'>
+            <img src={UpcommingEventIcon} />
+            <h3 className='font-[700] text-[30px] leading-[34.35px]'>Upcoming Events</h3>
+          </div>
+          <div className='mt-[37px] flex gap-[18px] flex-wrap'>
+            <UpcommingEventCard background={"#EC1F26"} />
+            <UpcommingEventCard background={"#F48589"} />
+            <UpcommingEventCard background={"#EC1F26"} />
+            <UpcommingEventCard background={"#F48589"} />
+            <UpcommingEventCard background={"#EC1F26"} />
+            <UpcommingEventCard background={"#F48589"} />
+          </div>
+        </div>
+      </div>
+
+
+      <div className='w-full min-h-[713px] bg-white'>
+        <div className='w-[1234px] xsm:w-[64.27083333333333vw] sm:w-[64.27083333333333vw] md:w-[64.27083333333333vw] lg:w-[64.27083333333333vw] min-w-[300px] min-h-[713px] m-auto pt-[83px] pb-[109px]'>
+          <div className='flex gap-[18px] items-center'>
+            <img src={JobListingsIcon} />
+            <h3 className='font-[700] text-[30px] leading-[34.35px]'>Job Listings</h3>
+          </div>
+          <div className='flex gap-[23.64px] flex-wrap mt-[41px]'>
+            {jobListingData?.map((job) => {
+              return <JobListingCard key={job.id} image={job.image} title={job.title} company={job.company} location={job.location} createdAt={job.createdAt} jobType={job.jobType} role={job.role} timing={job.timing} />
+            })
+            }
+          </div>
+        </div>
+      </div>
+
+
+      <div style={{background: `linear-gradient(270deg, #EC1F26 58.33%, rgba(236, 31, 38, 0) 84.56%), url(${FooterBackgroundImage})`}} className='w-full min-h-[287px] bg-white'>
+        <div className='flex xsm:items-start sm:items-start md:items-start items-center xsm:flex-col sm:flex-col md:flex-col xsm:gap-[20px] sm:gap-[20px] md:gap-[20px] gap-[138px] w-[1234px] xsm:w-[64.27083333333333vw] sm:w-[64.27083333333333vw] md:w-[64.27083333333333vw] lg:w-[64.27083333333333vw] min-w-[300px] min-h-[287px] m-auto pt-[83px] pb-[109px]'>
+          <h3 className='font-[700] text-[clamp(28px,2.2916666666666665vw,44px)] leading-[54.12px] xsm:w-[36.822916666666664vw] sm:w-[36.822916666666664vw] md:w-[36.822916666666664vw] w-[707px] min-w-[300px] text-white'>Join our community! Create
+            profile pages & connect to local
+            or global opportunities</h3>
+            <button className='w-[13.385416666666666vw] min-w-[180px] h-[54px] bg-white rounded-[9px] font-[400] text-[18px] leading-[20.7px] text-primary'>Sign Up</button>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       fontFamily: {
         'poppins': ['Poppins', "sans-serif"],
+      },
+      colors:{
+        'primary':'#EC1F26'
       }
     },
     screens: {
