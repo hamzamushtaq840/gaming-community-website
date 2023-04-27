2b5d6934d67f948e69d3d70ad553a5691138c6af
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-04-27 15:53:29 +0500

    add-upcomming events and featured projects

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import GoalsCard from '../components/Community/GoalsCard'
+import UpcommingEventIcon from '../assets/UpcommingEventIcon.svg'
 import { communityGoalsCardsData } from "../utils/consts.js"
+import FeaturedProjectsCard from '../components/Community/FeaturedProjectsCard'
+import UpcommingEventCard from '../components/Community/UpcommingEventCard'
 
 export default function Community() {
   return (
@@ -25,7 +28,36 @@ export default function Community() {
             <GoalsCard key={goal.id} background={goal.background} image={goal.image} heading={goal.title} description={goal.description} buttonTitle={goal.buttonTitle} />
           )}
         </div>
+      </div>
 
+      <div className='w-full min-h-[940px] bg-white'>
+        <div className='w-[1234px] xsm:w-[64.27083333333333vw] sm:w-[64.27083333333333vw] md:w-[64.27083333333333vw] lg:w-[64.27083333333333vw] min-w-[300px] min-h-[940px] m-auto pt-[87px] pb-[118px]'>
+          <h1 className='font-[700] text-[40px] leading-[44.2px] text-center mb-[16px]'>Featured Projects</h1>
+          <p className='font-[400] text-[18px] leading-[25.11px] text-center tracking-[-4%] w-[632px] min-w-[300px] m-auto xsm:w-[32.916666666666664vw] sm:w-[32.916666666666664vw] md:w-[32.916666666666664vw]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+          <div className='mt-[64px] w-full flex gap-[23px] justify-center flex-wrap'>
+            <FeaturedProjectsCard background={"./featureProject1.png"} content={"Lorem ipsum dolor sit amet, consectetur adipiscing"} />
+            <FeaturedProjectsCard background={"./featureProject2.png"} content={"Lorem ipsum dolor sit amet, consectetur adipiscing"} />
+            <FeaturedProjectsCard background={"./featureProject3.png"} content={"Lorem ipsum dolor sit amet, consectetur adipiscing"} />
+          </div>
+        </div>
+      </div>
+
+
+      <div className='w-full min-h-[1087px] bg-[#F5F5F5]'>
+        <div className='w-[1234px] xsm:w-[64.27083333333333vw] sm:w-[64.27083333333333vw] md:w-[64.27083333333333vw] lg:w-[64.27083333333333vw] min-w-[300px] min-h-[940px] m-auto pt-[73px] pb-[108px]'>
+            <div className='flex gap-[18px] items-center'>
+              <img src={UpcommingEventIcon}/>
+              <h3 className='font-[700] text-[30px] leading-[34.35px]'>Upcoming Events</h3>
+            </div>
+            <div className='mt-[37px] flex gap-[18px] flex-wrap'>
+              <UpcommingEventCard background={"#EC1F26"}/>
+              <UpcommingEventCard background={"#F48589"}/>
+              <UpcommingEventCard background={"#EC1F26"}/>
+              <UpcommingEventCard background={"#F48589"}/>
+              <UpcommingEventCard background={"#EC1F26"}/>
+              <UpcommingEventCard background={"#F48589"}/>
+            </div>
+        </div>
       </div>
     </div>
   )
