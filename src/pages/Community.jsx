b40d2d47b0c924b9cf77d2ca4c24bce5472a9f76
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-04-27 11:10:50 +0500

    add-responsive to offer and acheived goals section

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -4,8 +4,8 @@ import { NavLink, Outlet } from 'react-router-dom'
 export default function Community() {
   return (
     <div>
-      <div className='bg-white h-[814px] w-full flex flex-col items-center'>
-        <div className='w-[360px] h-[33px] mt-[52px] mb-[48px] flex border-[2px] border-[#EC1F26] rounded-[16.5px] overflow-hidden'>
+      <div className='bg-white min-h-[814px] w-full flex flex-col items-center'>
+        <div className='w-[18.75vw] min-h-[33px] min-w-[300px] h-[33px] mt-[52px] mb-[48px] flex border-[2px] border-[#EC1F26] rounded-[16.5px] overflow-hidden'>
           <NavLink to={"/"} className={({ isActive }) => isActive ? "bg-[#EC1F26] text-white flex-1 flex justify-center" : "text-[#EC1F26] bg-white flex-1 flex justify-center"}>
             <button className='font-[400] text-[14px] leading-[16.1px]'>Achieved Goals</button>
           </NavLink>
