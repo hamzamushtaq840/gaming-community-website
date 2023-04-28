f2567aa182e5e1635999ea5befdb6f2c4dea63ab
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2023-04-28 20:35:11 +0500

    conflict-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Community from "./pages/Community"
 import CommunityNav from "./layout/CommunityNav"
 import AchievedGoals from "./components/Community/AchievedGoals"
 import OfferOpportunities from "./components/Community/OfferOpportunities"
+import SigninCommunity from "./pages/SigninCommunity"
+import SigninMain from "./pages/SigninMain"
+import SignupCommunity from "./pages/SignupCommunity"
+import SignupMain from "./pages/SignupMain"
 
 export default function App() {
   return (
@@ -15,7 +19,10 @@ export default function App() {
         <Route path="/signin-community/:communityId" element={<SigninCommunity />}></Route>
         {/* Main Page */}
         <Route element={<CommunityNav></CommunityNav>}>
-          <Route path="/" element={<Community />}></Route>
+          <Route path="/" element={<Community />}>
+            <Route index element={<AchievedGoals></AchievedGoals>}></Route>
+            <Route path="offer-opportunities" element={<OfferOpportunities></OfferOpportunities>}></Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
