ce3534a50f2a5d9a206196029f592c41a44eec84
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-04-30 19:35:48 +0500

    added simple manage page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Profile from "./pages/Profile"
 import Layout from "./layout/Layout"
 import TeamProfile from "./pages/TeamProfile"
 import ManageProfile from "./components/Profile/ManageProfile"
+import ViewProfile from "./components/Profile/ViewProfile"
 
 export default function App() {
   return (
@@ -33,6 +34,7 @@ export default function App() {
         <Route element={<Layout></Layout>}>
           <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
           <Route path="/profile" element={<Profile></Profile>}>
+            <Route index element={<ViewProfile></ViewProfile>}></Route>
             <Route path="manage" element={<ManageProfile></ManageProfile>}></Route>
           </Route>
           <Route path="/team-profile" element={<TeamProfile />}></Route>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Profile() {
   return (
-    <div className='text-black dark:text-white'>Profile</div>
+    <div className='dark:text-white dark:bg-black pr-[10.989583333333334vw] pl-[10.9375vw]'>
+      
+      <div className='flex gap-[11px] pt-[71px] pb-[62px]'>
+        <button className='w-[200px] h-[48px] bg-[rgba(255,255,255,0.25)] rounded-[9px]'>View Profile</button>
+        <button className='w-[200px] h-[48px] bg-[rgba(255,255,255,0.25)] rounded-[9px]'>Analytics</button>
+        <button className='w-[200px] h-[48px] bg-[rgba(255,255,255,0.25)] rounded-[9px]'>Pages</button>
+        <button className='w-[200px] h-[48px] bg-[rgba(255,255,255,0.25)] rounded-[9px]'>Share</button>
+      </div>
+      <div>
+        <Outlet/>
+      </div>
+    </div>
   )
 }
